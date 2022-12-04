374a78c03b58eb7e3b370e914ecf98bc22465549
Don Isaac <donald.isaac@gmail.com>
2022-12-04 14:50:58 -0500

    refactor: top level index exports

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,48 @@
 // export * from './useFetch/types'
 
-export { default as useDelayedCallback } from './useDelayedCallback'
-export type {
+import {
+    default as useDelayedCallback,
     DelayedCallbackOptions,
     DelayStrategy,
 } from './useDelayedCallback'
+import { default as useDidMount } from './useDidMount'
+import { default as useFetch } from './useFetch'
+import { default as useForceUpdate } from './useForceUpdate'
+import { default as useInterval } from './useInterval'
+import { default as useLocalStorage } from './useLocalStorage'
+import { default as useMeasuredCallback } from './useMeasuredCallback'
+import { default as useMemoCompare } from './useMemoCompare'
+import { default as useMount } from './useMount'
+import {
+    default as usePaginated,
+    UsePaginatedOptions,
+    PaginatedRequest,
+} from './usePaginated'
+import { RequestState, RequestStatus } from './types'
 
-export { default as useFetch } from './useFetch'
-export * from './useFetch'
-export { default as useDidMount } from './useDidMount'
-export { default as useMount } from './useMount'
-export { default as useForceUpdate } from './useForceUpdate'
-export { default as useInterval } from './useInterval'
-export { default as useMeasuredCallback } from './useMeasuredCallback'
-export { default as useMemoCompare } from './useMemoCompare'
-export { default as usePaginated } from './usePaginated'
-export { default as useLocalStorage } from './useLocalStorage'
-export type { UsePaginatedOptions, PaginatedRequest } from './usePaginated'
+// Hooks
+export {
+    useDelayedCallback,
+    useDidMount,
+    useFetch,
+    useForceUpdate,
+    useInterval,
+    useLocalStorage,
+    useMeasuredCallback,
+    useMemoCompare,
+    useMount,
+    usePaginated,
+}
+
+// Types
+export type {
+    // useDelayedCallback
+    DelayedCallbackOptions,
+    DelayStrategy,
+    // usePaginated
+    PaginatedRequest,
+    UsePaginatedOptions,
+    // useFetch
+    RequestState,
+    RequestStatus,
+}
